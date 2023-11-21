f1133b0d5aa575f57d648fffdfb95eb73ef09266
19Pawel97 <19pawel9772@gmail.com>
2023-11-21 19:37:18 +0100

    Adding token based dowhile

--- src/main/java/Exceptions/Main.java
+++ src/main/java/Exceptions/Main.java
@@ -1,11 +1,32 @@
 package Exceptions;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        Scanner scanner = new Scanner(System.in);
+        askForNumber(scanner);
 
     }
 
+    private static int askForNumber(Scanner scanner) {
+
+        Integer value;
+
+        do {
+            try {
+                System.out.println("Enter a number: ");
+                value = scanner.nextInt();
+            } catch (InputMismatchException ime) {
+                System.out.println("The input is incorrect, enter a NUMBER: ");
+                scanner.next();
+                value = null;
+            }
+        } while (value == null);
+
+        return value;
+    }
 }
